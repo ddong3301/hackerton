9eb0eadc667ca088c0d2ab0146674a46ac6f82ea
Dong <ssuny303@gmail.com>
2021-10-12 15:56:23 +0900

    asd

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.set('view engine', 'ejs');
 app.use(express.json());
 app.use(cookieParser());
 app.use(express.urlencoded({extended: false}));
-app.use(express.static(path.join(__dirname + '../')));
+app.use(express.static(path.join(__dirname, '../')));
 
 app.use('/', userRouter);
 app.use('/', uploadRouter);
