5c7b198ebdc0a0f7b9fdf4f4bbbbd6ca2e30d8e6
Dong <ssuny303@gmail.com>
2021-10-12 16:54:28 +0900

    asd

--- src/controllers/displayCtrl.js
+++ src/controllers/displayCtrl.js
@@ -3,8 +3,9 @@ const Photo = require('../models/Photo');
 const getSingleImageFromDB = (req, res) => {
     Photo.display_Photo().then((path) => {
         console.log(path[0].filePath);
-        res.render('display', { photo: path[0].filePath });
+        //res.render('display', { photo: path[0].filePath });
         //res.render('display', {photo : "2021-10-12T13-43-34.442Z-dog.jpg"})
+        res.send({photo : path[0].filePath})
     })
 }
 
