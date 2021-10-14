a3b5b0531de54f50daa5487f1bc74113feca7e0f
Dong <ssuny303@gmail.com>
2021-10-14 15:50:38 +0900

    21-10-14-1

--- src/controllers/displayCtrl.js
+++ src/controllers/displayCtrl.js
@@ -4,7 +4,6 @@ const getSingleImageFromDB = (req, res) => {
     Photo.display_Photo().then((path) => {
         console.log(path[0].filePath);
         //res.render('display', { photo: path[0].filePath });
-        //res.render('display', {photo : "2021-10-12T13-43-34.442Z-dog.jpg"})
         res.send({photo : path[0].filePath});
     })
 }
