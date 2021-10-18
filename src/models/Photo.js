5d7d7b415cfaccc1b3df049b32d28278c87970f6
Dong <ssuny303@gmail.com>
2021-10-18 22:56:52 +0900

    test

--- src/models/Photo.js
+++ src/models/Photo.js
@@ -6,7 +6,7 @@ const date = new Date();
 // todo: 애들 insert into 객체형식으로 넣는 방법 기억나는지 확인하기
 const insert_Photo = (parameters) => {
     return new Promise((resolve, reject) => {
-        db.query(`INSERT INTO photo SET fileName = '${parameters.fileName}', simplePath = '${parameters.simplePath}', fullPath = '${parameters.fullPath}', fileType = '${parameters.fileType}', g_num = '${parameters.g_num}'`, (err, db_data) => {
+        db.query(`INSERT INTO photo SET fileName = '${parameters.fileName}', filePath = '${parameters.simplePath}', fileType = '${parameters.fileType}', g_num = '${parameters.g_num}'`, (err, db_data) => {
             if (err) {
                 reject(err);
             } else {
