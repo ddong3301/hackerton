4fc43883706268f8fe65f0411f9a3c8189311079
Dong <ssuny303@gmail.com>
2021-10-21 08:28:59 +0900

    asd

--- src/middleware/push.js
+++ src/middleware/push.js
@@ -1,42 +1,39 @@
-const apn = require('apn');
+// const apn = require('apn');
 
-// Developer mode
-var dev_options = {
-    gateway : "gateway.sandbox.push.apple.com",
-    cert: './ios-certificates/development/swift_apns_development_cert.pem',
-    key: './ios-certificates/development/swift_apns_development_server.pem',
-    production: false
-};
-
-// Production(App store)
-// var pro_options = {
-//     gateway : "gateway.push.apple.com",
-//     cert: './ios-certificates/development/swift_apns_production_cert.pem',
-//     key: './ios-certificates/development/swift_apns_production_server.pem',
-//     production: true
+// // Developer mode
+// var dev_options = {
+//     gateway : "gateway.sandbox.push.apple.com",
+//     cert: './ios-certificates/development/swift_apns_development_cert.pem',
+//     key: './ios-certificates/development/swift_apns_development_server.pem',
+//     production: false
 // };
 
-var apnConnection = new apn.Connection(dev_options);
+// // Production(App store)
+// // var pro_options = {
+// //     gateway : "gateway.push.apple.com",
+// //     cert: './ios-certificates/development/swift_apns_production_cert.pem',
+// //     key: './ios-certificates/development/swift_apns_production_server.pem',
+// //     production: true
+// // };
 
-var note = new apn.Notification();
-note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
-note.badge = 1;
-note.sound = "ping.aiff";
-note.alert = "Swift프로젝트에서 Push 메시지 수신!";
+// var apnConnection = new apn.Connection(dev_options);
 
-var tokenArr =
-    [
-        '수집된 디바이스 토큰값',
-    ];
-var deviceArr = [];
+// var note = new apn.Notification();
+// note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
+// note.badge = 1;
+// note.sound = "ping.aiff";
+// note.alert = "Swift프로젝트에서 Push 메시지 수신!";
 
-for(var i=0;i<tokenArr.length;i++){
-    var token = tokenArr[i];
-    var myDevice = new apn.Device(token);
-    deviceArr.push(myDevice); 
-} 
+// var tokenArr =
+//     [
+//         '수집된 디바이스 토큰값',
+//     ];
+// var deviceArr = [];
 
-apnConnection.pushNotification(note, deviceArr);
-
-module.exports = apnConnection;
+// for(var i=0;i<tokenArr.length;i++){
+//     var token = tokenArr[i];
+//     var myDevice = new apn.Device(token);
+//     deviceArr.push(myDevice); 
+// } 
+// apnConnection.pushNotification(note, deviceArr);
 
