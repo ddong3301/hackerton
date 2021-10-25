30d17c835a22712288d1caf7a4975ef28c933206
Dong <ssuny303@gmail.com>
2021-10-25 15:33:28 +0900

    asd

--- src/controllers/displayCtrl.js
+++ src/controllers/displayCtrl.js
@@ -4,16 +4,25 @@ const jwt = require('jsonwebtoken');
 const getImagesFromDB = (req, res) => {
     let token = req.cookies.x_auth;
     let decoded_token = jwt.verify(token, 'secret_key');
+    let asd;
     Photo.display_Gallery().then((data) => {
         for (var i = 0; i < data.length; i++) {
             if (decoded_token.region == data[i].photoRegion) {
-                console.log(data[i]);
-                res.send({ data: data[i]});
+                asd = [data];
+                
             }
         }
-
+        res.send(asd);
     });
 }
+
+// const getImagesFromDB = (req, res) => {
+//     let token = req.cookies.x_auth;
+//     let decoded_token = jwt.verify(token, 'secret_key');
+//     Photo.display_Gallery().then((data) => {
+//         res.send({ data: data});
+//     });
+// }
 
 module.exports = {
     getImagesFromDB,
--- src/middleware/push.js
+++ src/middleware/push.js
@@ -1,34 +1,36 @@
-require('dotenv').config({ path: ".env" });
+// require('dotenv').config({ path: ".env" });
 
-const sendNoti = () => {
-    const apn = require('apn');
+// const sendNoti = () => {
+const apn = require('apn');
 
-    var options = {
-        gateway: "gateway.sandbox.push.apple.com",
-        cert: "./cert/cert.pem",
-        key: "./cert/key.pem",
-        keyId : process.env.keyId,
-        teamId : process.env.teamId
-    };
-    
-    
-    var apnConnection = new apn.Provider(options);
-   
-    let deviceToken = process.env.deviceToken;
+var options = {
+    gateway: "gateway.sandbox.push.apple.com",
+    cert: "./cert/cert.pem",
+    key: "./cert/key.pem",
+    keyId: "Subeye key",
+    teamId: "JUNHYEOK GO",
+};
 
-    var note = new apn.Notification();
-    note.badge = 1;
-    note.sound = "default";
-    note.alert = '무임승차 적발';
-    note.topic = 'Twogudak.Subeye';
-    note.payload = { 'message': '푸쉬테스트입니다' };
-    
-    apnConnection.send(note, deviceToken).then((result) => {
-        console.log(result);
-    }).catch(function (err) {
-        throw (err);
-    });
-}
+console.log(options);
 
-module.exports = sendNoti;
+var apnConnection = new apn.Provider(options);
+
+let deviceToken = "b42f0038bda996cb120ea3dbf3bd8e81dd43efe30b77ec04eef26b9dfebb3c5a";
+
+var note = new apn.Notification();
+note.badge = 1;
+note.sound = "default";
+note.alert = '무임승차 적발';
+note.topic = 'Twogudak.Subeye';
+note.payload = { 'message': '푸쉬테스트입니다' };
+
+apnConnection.send(note, deviceToken).then((result) => {
+    console.log(result);
+}).catch(function (err) {
+    console.log(err);
+    throw (err);
+});
+// }
+
+// module.exports = sendNoti;
 
