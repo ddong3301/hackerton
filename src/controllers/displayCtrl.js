68c730b7b45996ae6055fd99e982d131606a7d2b
Dong <ssuny303@gmail.com>
2021-10-26 00:16:12 +0900

    asd

--- src/controllers/displayCtrl.js
+++ src/controllers/displayCtrl.js
@@ -12,22 +12,6 @@ const getImagesFromDB = (req, res) => {
     });
 }
 
-// const getImagesFromDB = (req, res) => {
-//     let token = req.cookies.x_auth;
-//     let decoded_token = jwt.verify(token, 'secret_key');
-//     Photo.display_Gallery().then((data) => {
-//         for (var i = 0; i < data.length; i++) {
-//             if (decoded_token.region == data[i].photoRegion) {
-//                 asd = data;
-
-//             }
-//         }
-//         console.log(asd);
-//         console.log(data);
-//         res.send({ data: data });
-//     });
-// }
-
 module.exports = {
     getImagesFromDB,
 }
