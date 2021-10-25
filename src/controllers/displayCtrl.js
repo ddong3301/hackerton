62e1182a3c9a404f3ee98d4632aa44cae5e40e58
Dong <ssuny303@gmail.com>
2021-10-25 14:45:41 +0900

    asd

--- src/controllers/displayCtrl.js
+++ src/controllers/displayCtrl.js
@@ -1,17 +1,26 @@
 const Photo = require('../models/Photo');
 const jwt = require('jsonwebtoken');
 
+// const getImagesFromDB = (req, res) => {
+//     let token = req.cookies.x_auth;
+//     let decoded_token = jwt.verify(token, 'secret_key');
+//     Photo.display_Gallery().then((data) => {
+//         for (var i = 0; i < data.length; i++) {
+//             if (decoded_token.region == data[i].photoRegion) {
+//                 console.log(data[i]);
+//                 res.send({ data: data[i] });
+//             }
+//         }
+
+//     });
+// }
+
 const getImagesFromDB = (req, res) => {
     let token = req.cookies.x_auth;
     let decoded_token = jwt.verify(token, 'secret_key');
     Photo.display_Gallery().then((data) => {
-        for (var i = 0; i < data.length; i++) {
-            // if (decoded_token.region == data[i].photoRegion) {
-                // console.log(data[i]);
-                res.send({ data: data });
-            // }
-        }
-
+        console.log(data);
+        res.send({ data: data });
     });
 }
 
